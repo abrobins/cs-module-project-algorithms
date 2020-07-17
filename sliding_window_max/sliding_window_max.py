970eb9d0f60548b77e57226d9cdd6cfc511907b5
abrobins <abrgroup@gmail.com>
2020-07-16 21:06:11 -0400

    added additional comments

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,14 +2,19 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+# look at using queue for this / dequeue (import from collections)
 
 
 def sliding_window_max(nums, k):
     output_list = []
     # Your code here
+    # loop through array from 0 - length of nums -k+1
     for i in range(0, len(nums)-k+1):
+        # create a sub list of nums from index to index + size of sliding window
         int_list = nums[i:i+k]
+        # add max number of new array to output_list
         output_list.append(max(int_list))
+    # return output list
     return output_list
 
 
